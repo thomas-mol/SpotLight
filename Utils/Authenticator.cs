415dfcb0b4d9d3382bd3d3277bfa5f99699844a8
Thomas Mol <s130286@ap.be>
2024-12-16 16:36:47 +0100

    Add project files.

---
+++ Utils/Authenticator.cs
@@ -0,0 +1,14 @@
+﻿using SpotLight.Models;
+
+namespace SpotLight.Utils
+{
+    public static class Authenticator
+    {
+        public static Token Token { get; set; }
+
+        public static async Task GetTokenAsync()
+        {
+
+        }
+    }
+}
